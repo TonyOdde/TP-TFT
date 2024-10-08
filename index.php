7f139d3424852e67707a72886cfe2984e00872a8
Tony-Odde <tony.odde@iut-dijon.u-bourgogne.fr>
2024-10-08 11:13:25 +0200

    init loader

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
+use Helpers\Psr4AutoloaderClass;
+
+require_once "Helpers/Psr4AutoloaderClass.php";
+
+$loader = new Psr4AutoloaderClass();
+$loader->register();
+$loader->addNamespace('\Helpers','/Helpers');
+
 echo "test";
